d5131b30887fdfa357c2e164f9cd09f9fd61745e
Sasidhar152 <131762529+Sasidhar152@users.noreply.github.com>
2023-07-06 11:17:27 +0530

    Delete selective programs directory

--- selective programs/2.py
+++
@@ -1,8 +0,0 @@
-a=int(input("enter first side:"))
-b=int(input("enter second side:"))
-c=int(input("enter third side:"))
-if( a+b>c and a+c>b and b+c>a):
-      print("its a triangle")
-else:
-        print("its not a triangle")
-      
--- selective programs/6.py
+++
@@ -1,8 +0,0 @@
-name =input("Enter your name: ")
-age = int(input("Enter your year of birth: "))
-w = int(input("Enter your weight: "))
-c=2023-age
-if (c>=18 and w>40):
-    print('1')
-else:
-    print("0")
